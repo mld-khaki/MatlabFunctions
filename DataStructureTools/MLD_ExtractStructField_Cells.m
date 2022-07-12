ddf98aa6c2bcc95c4f11f68bd1f86c8172b4b259
mld-khaki <milad.khaki@gmail.com>
2022-07-12 13:56:44 -0400

    Update MLD_ExtractStructField_Cells.m

--- DataStructureTools/MLD_ExtractStructField_Cells.m
+++ DataStructureTools/MLD_ExtractStructField_Cells.m
@@ -1,8 +1,10 @@
 function Array = MLD_ExtractStructField_Cells(myStruct,FieldName)
 for mCtr=1:length(myStruct)
-	if mCtr==1
+	
+    if mCtr==1
 		Array = {};
-	end
+    end
+
 	if  isempty(myStruct(mCtr).(FieldName)) || ...
 			nansum(MLD_IsNan(myStruct(mCtr).(FieldName)))>0
 		Array{mCtr} = '';
