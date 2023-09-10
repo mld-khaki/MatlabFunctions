5a6cabd470aaaf6d4302eabe195f0bf352713a91
Milad Khaki <milad.khaki@gmail.com>
2023-09-10 19:05:43 -0400

    Minor bugs fixed

--- DataMemory/MLD_listStructSizes.m
+++ DataMemory/MLD_listStructSizes.m
@@ -1,54 +1,60 @@
 function [varNames, varSizes] = MLD_listStructSizes(s, prefix, doPlot)
-    if nargin < 2
-        prefix = 's';
+% data.a = ones(10, 10);
+% data.b.subfield1 = zeros(5, 5);
+% data.b.subfield2 = 2;
+%
+% MLD_listStructSizes(data, 'data', true);
+
+if nargin < 2
+    prefix = 's';
+end
+
+if nargin < 3
+    doPlot = false;
+end
+
+% Check if the input is a struct
+if ~isstruct(s)
+    error('Input must be a struct.');
+end
+
+fields = fieldnames(s);
+varNames = "";
+varSizes = [];
+
+for iCtr = 1:length(fields)
+    field = fields{iCtr};
+    value = s.(field);
+    sizeValue = numel(value);
+
+    % Store the variable name and size for plotting
+    varNames(iCtr) = strrep(string([prefix '.' field]),"_","\_");
+    varSizes(iCtr) = sizeValue;
+
+    % Print the variable name and size
+    sizeStr = num2str(sizeValue);
+    disp([prefix '.' field ': ' sizeStr]);
+
+    % Recursively list sizes if the field is also a struct
+    if isstruct(value)
+        [subVarNames, subVarSizes] = MLD_listStructSizes(value, [prefix '.' field], false);
+        varNames = [varNames, subVarNames];
+        varSizes = [varSizes, subVarSizes];
     end
+end
 
-    if nargin < 3
-        doPlot = false;
-    end
+if doPlot
+    figure;
+    pltVarSizes = varSizes >= 0.25*nanmean(varSizes);
+    bar(varSizes(pltVarSizes));
 
-    % Check if the input is a struct
-    if ~isstruct(s)
-        error('Input must be a struct.');
-    end
+    set(gca, 'XTickLabel', varNames(pltVarSizes), 'XTick', 1:numel(varNames(pltVarSizes)));
+    xlabel('Number of Elements');
+    title('Variable Sizes in Struct');
+end
 
-    fields = fieldnames(s);
-    varNames = "";
-    varSizes = [];
-
-    for iCtr = 1:length(fields)
-        field = fields{iCtr};
-        value = s.(field);
-        sizeValue = numel(value);
-
-        % Store the variable name and size for plotting
-        varNames(iCtr) = strrep(string([prefix '.' field]),"_","\_");
-        varSizes(iCtr) = sizeValue;
-
-        % Print the variable name and size
-        sizeStr = num2str(sizeValue);
-        disp([prefix '.' field ': ' sizeStr]);
-
-        % Recursively list sizes if the field is also a struct
-        if isstruct(value)
-            [subVarNames, subVarSizes] = MLD_listStructSizes(value, [prefix '.' field], false);
-            varNames = [varNames, subVarNames];
-            varSizes = [varSizes, subVarSizes];
-        end
-    end
-
-    if doPlot
-        figure;
-        pltVarSizes = varSizes >= 0.25*nanmean(varSizes);
-        bar(varSizes(pltVarSizes));
-        
-        set(gca, 'XTickLabel', varNames(pltVarSizes), 'XTick', 1:numel(varNames(pltVarSizes)));
-        xlabel('Number of Elements');
-        title('Variable Sizes in Struct');
-    end
-
-    if nargout > 0
-        varargout{1} = varNames;
-        varargout{2} = varSizes;
-    end
+if nargout > 0
+    varargout{1} = varNames;
+    varargout{2} = varSizes;
+end
 end
--- DataStructureTools/MLD_removeFieldsStartingWith.m
+++ DataStructureTools/MLD_removeFieldsStartingWith.m
@@ -1,24 +1,32 @@
 function s = MLD_removeFieldsStartingWith(s, InpStr)
-    % Check if the input is a struct
-    if ~isstruct(s)
-        error('Input must be a struct.');
+% data.Inp_a = ones(10, 10);
+% data.b.subfield1 = zeros(5, 5);
+% data.b.Inp_subfield2 = 2;
+% data.b.c.Inp_d = 3;
+% data.b.c.e = 4;
+%
+% cleaned_data = MLD_removeFieldsStartingWith(data, 'Inp_');
+
+% Check if the input is a struct
+if ~isstruct(s)
+    error('Input must be a struct.');
+end
+
+fields = fieldnames(s);
+
+for i = 1:length(fields)
+    field = fields{i};
+    value = s.(field);
+
+    % If the field name starts with InpStr, remove it
+    if startsWith(lower(field), lower(InpStr))
+        s = rmfield(s, field);
+        continue; % No need to check this field's contents
     end
 
-    fields = fieldnames(s);
-
-    for i = 1:length(fields)
-        field = fields{i};
-        value = s.(field);
-
-        % If the field name starts with InpStr, remove it
-        if startsWith(lower(field), lower(InpStr))
-            s = rmfield(s, field);
-            continue; % No need to check this field's contents
-        end
-
-        % Recursively remove fields if the current field is also a struct
-        if isstruct(value)
-            s.(field) = MLD_removeFieldsStartingWith(value, InpStr);
-        end
+    % Recursively remove fields if the current field is also a struct
+    if isstruct(value)
+        s.(field) = MLD_removeFieldsStartingWith(value, InpStr);
     end
+end
 end
